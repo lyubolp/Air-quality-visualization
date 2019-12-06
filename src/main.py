bde6ba5f60ed2c4fb2546d131703c363a127e01b
Lyuboslav Karev <lyubo.karev@gmail.com>
2019-12-06 16:41:15 +0200

    Added some temp code

--- src/main.py
+++ src/main.py
@@ -1,38 +1,42 @@
 from src.CsvParser import CsvParser, Parameter, Station
+from datetime import datetime
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, matplotlib.dates
 from matplotlib.widgets import Slider, Button, RadioButtons
 
-
 s = CsvParser.CsvParser("data/data.csv")
+result = s.get([Station.Kopitoto], [Parameter.Temperature], datetime(2016, 1, 21, 0), datetime(2016, 1, 22, 0))
 
-"""
-Get for all stations Pressure data
-"""
-r = s.get([Station.All], [Parameter.CO])
+dates = []
+values = []
+for i in result:
+    dates.append(i[0])
+    values.append(i[3])
+
+# convert the dates to matplotlib format
+dates = matplotlib.dates.date2num(dates)
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
 t = np.arange(0.0, 1.0, 0.001)
+
 a0 = 5
 f0 = 3
-delta_f = 5.0
-s = a0 * np.sin(2 * np.pi * f0 * t)
-l, = plt.plot(t, s, lw=2)
+l, = plt.plot_date(dates, values, markersize=3)
 ax.margins(x=0)
 
 axcolor = 'lightgoldenrodyellow'
 axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
 
-sfreq = Slider(axfreq, 'Freq', 0.1, 30.0, valinit=f0, valstep=delta_f)
+sfreq = Slider(axfreq, 'Дата', 0.1, 30.0, valinit=f0, valstep=1)
 samp = Slider(axamp, 'Amp', 0.1, 10.0, valinit=a0)
 
 
 def update(val):
     amp = samp.val
     freq = sfreq.val
-    l.set_ydata(amp*np.sin(2*np.pi*freq*t))
+    l.set_ydata(s)
     fig.canvas.draw_idle()
 
 
@@ -46,6 +50,8 @@ button = Button(resetax, 'Reset', color=axcolor, hovercolor='0.975')
 def reset(event):
     sfreq.reset()
     samp.reset()
+
+
 button.on_clicked(reset)
 
 rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
@@ -55,6 +61,8 @@ radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
 def colorfunc(label):
     l.set_color(label)
     fig.canvas.draw_idle()
+
+
 radio.on_clicked(colorfunc)
 
 plt.show()
