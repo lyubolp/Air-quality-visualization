6648ab5d4ac8002c49516c138302e0ff0d062f4d
Luchev <luchevz@gmail.com>
2019-12-09 21:10:58 +0200

    Refactor parser - remove redundant code
    parser:
    	Refactor the methods convert_station_list
    	and convert_parameter_list to use one unified function
    	convert_enum_key_list_to_enum_value_list to parse their
    	parameters
    
    	Improve comments on methods to clarify what the functionality is
    	exactly

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -7,21 +7,31 @@ class Parser:
         self.data = []
 
     @staticmethod
+    def convert_enum_key_list_to_enum_value_list(items: list, enum_type):
+        """
+        Convert a list of Enum keys to a list of Enum values
+        If the provided list contains invalid objects return empty list
+
+        IMPORTANT: Works only with IntEnums which have All key, as the ones in this project
+        """
+        out = []
+        for i in items:
+            if isinstance(i, enum_type):
+                out.append(i.value)
+            else:
+                return []
+        if enum_type.All.value in out:
+            out = list(map(int, enum_type))
+            out.remove(enum_type.All.value)
+        return out
+
+    @staticmethod
     def convert_station_list(station: list) -> list:
         """
         Convert a list of Station enums to a list of ints
         If the provided list contains invalid objects return empty list
         """
-        out = []
-        for i in station:
-            if isinstance(i, Station):
-                out.append(i.value)
-            else:
-                return []
-        if Station.All.value in out:
-            out = list(map(int, Station))
-            out.remove(Station.All.value)
-        return out
+        return Parser.convert_enum_key_list_to_enum_value_list(station, Station)
 
     @staticmethod
     def convert_parameter_list(parameter: list) -> list:
@@ -29,18 +39,7 @@ class Parser:
         Convert a list of Parameter enums to a list of ints
         If the provided list contains invalid objects return empty list
         """
-        out = []
-        for i in parameter:
-            if isinstance(i, Parameter):
-                out.append(i.value)
-            else:
-                return []
-
-        if Parameter.All.value in out:
-            out = list(map(int, Parameter))
-            out.remove(Parameter.All.value)
-
-        return out
+        return Parser.convert_enum_key_list_to_enum_value_list(parameter, Parameter)
 
     def get(self, stations: list, parameters: list,
             start: datetime = None, end: datetime = None) -> list:
@@ -48,6 +47,8 @@ class Parser:
         Given a datetime, a list of stations and a list of parameters
         return the data which matches these parameters for these stations and
         that time
+
+        If no end date is provided the assumed end date is the same day, i.e the given period is 1 day
         """
         stations = self.convert_station_list(stations)
         parameters = self.convert_parameter_list(parameters)
