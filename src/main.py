4a1c7cfcaf98b86c1460cc3013f24b9232a62293
Lyuboslav Karev <lyubo.karev@gmail.com>
2019-12-13 16:34:45 +0200

    Finished work on the time-range plot

--- src/main.py
+++ src/main.py
@@ -3,4 +3,5 @@ from src.enums.enums import Station, Parameter
 from datetime import datetime
 if __name__ == "__main__":
     plotter = Plotter("data/data.csv")
-    plotter.plot_single_region_day(Station.Druzhba, Parameter.CO, datetime(2017, 1, 21, 0))
+    plotter.plot_single_region_time_range(Station.Druzhba, Parameter.Temperature,
+                                          datetime(2017, 1, 21, 0), datetime(2017, 1, 25, 0))
